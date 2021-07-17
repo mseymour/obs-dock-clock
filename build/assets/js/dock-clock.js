9615e547b9ca388e706450259211a86f90640fe0
Mark Seymour <mark.seymour.ns@gmail.com>
2021-07-16 22:36:41 -0300

    Promise-driven setup, generate initial clock display

--- build/assets/js/dock-clock.js
+++ build/assets/js/dock-clock.js
@@ -17,37 +17,87 @@ export default class DockClock {
     const default_options = {
       address: 'localhost:4444',
       password: '',
-      interval_ms: 500,
+      interval_ms: 1000,
     };
 
     this.element = element;
-    this.options = Object.assign({}, options, default_options);
+    this.options = Object.assign({}, default_options, options);
 
     this.obs = new OBSWebSocket();
 
-    this.intervalMilliseconds = this.options.interval_ms;
+    this.clocks = [];
+
     this.interval = null;
+
+    this.setup();
   }
 
   /**
    * Starts the clock
    */
   setup() {
+    let self = this;
+
     this.obs.connect({
       address: this.options.address,
       password: this.options.password,
-    }).then(function () {
-      this.interval = setInterval(function () {
-        this.tick();
-      }, this.intervalMilliseconds);
+    })
+    .then(() => {
+      // Set up clock
+      this.clockSetUp();
+    })
+    .then(() => {
+      // Set up tick
+      let self = this;
+      this.interval = setInterval(() => {
+        self.tick();
+      }, this.options.interval_ms);
+    })
+    .catch(err => {
+        console.error('Promise error:', err);
     });
+
+    this.obs.on('error', err => {
+      console.error('Socket error:', err);
+    });
+  }
+
+  /**
+   * Set up individual clock
+   */
+  clockSetUp() {
+    // Clock container, display
+    let container = document.querySelector('#clock'),
+        display;
+    // Clock element templates
+    const format            = 'h:ms',
+          displayTemplate   = document.querySelector('#tmp-clock'),
+          elementTemplate   = document.querySelector('#tmp-clock-element'),
+          separatorTemplate = document.querySelector('#tmp-clock-separator');
+
+    display = displayTemplate.content.cloneNode(true);
+
+    [...format].forEach(char => {
+      if (char === ':') {
+        display.querySelector('.clock__display').appendChild(separatorTemplate.content.cloneNode(true));
+      } else {
+        let clone   = elementTemplate.content.cloneNode(true),
+            element = clone.querySelector('.clock__element');
+        element.setAttribute('data-element-type', char);
+        display.querySelector('.clock__display').appendChild(clone);
+      }
+    });
+
+    display.querySelector('.clock__label').textContent = 'Local Time';
+
+    container.appendChild(display);
   }
 
   /**
    * A single clock tick.
    */
   tick() {
-    console.log('tick!');
+    console.log(this.obs);
   }
 
   /**
--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,9 +1,9 @@
 /*globals config*/
 import DockClock from './dock-clock';
 
-document.addEventListener(() => {
+document.addEventListener('DOMContentLoaded', () => {
   /* jshint unused:false */
-  const dock_clock = new DockClock(document.getElementByID('clock'), {
+  const dock_clock = new DockClock(document.getElementById('clock'), {
     address: config.address,
     password: config.password,
   });
